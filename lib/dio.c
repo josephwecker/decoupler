83c44b97ffb14e619e889591f0092fb779931d40
Joseph Wecker <jwecker@justin.tv>
2012-02-21 17:35:15 -0800

    Rethinking the overall flow...

--- lib/dio.c
+++ lib/dio.c
@@ -1,7 +1,3 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <stddef.h>
-#include <stdint.h>
 
 #include "dio.h"
 
--- src/dcpl.c
+++ src/dcpl.c
@@ -1,7 +1,24 @@
-#include <stdio.h>
 #include "dio.h"
 
+/* TODO:
+ *  - [ ] Failure modes / esp. if there are still listeners...
+ *
+ * STATES:
+ *  decoupler running?
+ *  input fifo exists?
+ *  decoupler bound to appropriate unix socket?
+ *  
+ *
+ * - Expecting
+ * - Active
+ * - Flushing
+ * - Dormant    | decoupler not running
+ * - Final      | decoupler not running
+ */
+
+
+
 int main(int argc, char *argv[]) {
-    printf("check: \n");
+
     return 0;
 }
